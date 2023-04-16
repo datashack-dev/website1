ec7e68ad6073fb90a0c00ad218035c9496d5ee10
Admin <admin@Admins-MacBook-Pro.local>
2023-04-17 00:11:54 +0300

    wip notebook section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
-  ],
+  content: ['*.html', '_site/*.html','_site/**/*.html', '_pages/**/*.md', '_posts/**/*.md', '_layouts/**/*.html', '_includes/**/*.html'],
   theme: {
-    theme: {
-      extend: {},
-    },
+    extend: {
+      colors: {
+        gray: colors.slate,
+        primary: colors.blue,
+        secondary: colors.fuchsia,
+        tertiary: colors.slate,
+        danger: colors.red,
+      },
+      fontFamily: {
+        sans: ['Work Sans','Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: []
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio')
+  ]
 }
